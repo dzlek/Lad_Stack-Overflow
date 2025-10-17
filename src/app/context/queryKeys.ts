3df6161652433551d45b12d2240c37bb05670957
dzlek <6894255@gmail.com>
2025-10-17 09:41:24 +0300

    feat: add PostPage

--- src/app/context/queryKeys.ts
+++ src/app/context/queryKeys.ts
@@ -1,3 +1,4 @@
 export const QUERY_KEYS = {
   SNIPPETS: ['snippets'],
+  SNIPPET_DETAILS: (id: string) => ['snippet', id],
 };
---
+++ src/pages/postPage/PostPage.tsx
@@ -0,0 +1,90 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useQueryClient, useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+import { QUERY_KEYS } from '../../app/context/queryKeys';
+import CommentsList from '../../components/commentList/CommentsList';
+import CommentForm from '../../components/commentForm/CommentForm';
+import SnippetCard, {
+  Snippet,
+  Comment,
+} from '../../components/snippetCard/SnippetCard';
+import { getSocket } from '../../app/context/useSocket';
+import { useAuth } from '../../app/context/useAuth';
+
+const fetchSnippet = async (id: string): Promise<Snippet> => {
+  const res = await axios.get(`/api/snippets/${id}`);
+  return res.data.data;
+};
+
+const PostPage = () => {
+  const { id } = useParams<{ id: string }>();
+  const queryClient = useQueryClient();
+
+  const { isAuth } = useAuth();
+
+  const { data: snippet, isLoading } = useQuery<Snippet>({
+    queryKey: QUERY_KEYS.SNIPPET_DETAILS(id!),
+    queryFn: () => fetchSnippet(id!),
+    enabled: !!id,
+  });
+
+  useEffect(() => {
+    if (!id) return;
+
+    const socket = getSocket();
+
+    const handleNewComment = (payload: {
+      comment: Comment;
+      snippetId: string;
+    }) => {
+      if (payload.snippetId !== id) return;
+
+      queryClient.setQueryData<Snippet>(
+        QUERY_KEYS.SNIPPET_DETAILS(id),
+        (oldSnippet) => {
+          if (!oldSnippet) return oldSnippet;
+          return {
+            ...oldSnippet,
+            comments: [...oldSnippet.comments, payload.comment],
+          };
+        },
+      );
+    };
+
+    socket.on('comment:created', handleNewComment);
+
+    return () => {
+      socket.off('comment:created', handleNewComment);
+    };
+  }, [id, queryClient]);
+
+  if (isLoading) return <p>Loading post...</p>;
+  if (!snippet) return <p>Snippet not found</p>;
+
+  const handleLocalAdd = (newComment: Comment) => {
+    if (!id) return;
+    queryClient.setQueryData<Snippet>(
+      QUERY_KEYS.SNIPPET_DETAILS(id),
+      (oldSnippet) => {
+        if (!oldSnippet) return oldSnippet;
+        return {
+          ...oldSnippet,
+          comments: [...oldSnippet.comments, newComment],
+        };
+      },
+    );
+  };
+
+  return (
+    <div>
+      <SnippetCard snippet={snippet} isAuth={true} currentUser={null} />
+
+      <CommentsList comments={snippet.comments} />
+      {isAuth && <CommentForm snippetId={id!} onSuccess={handleLocalAdd} />}
+    </div>
+  );
+};
+
+export default PostPage;
